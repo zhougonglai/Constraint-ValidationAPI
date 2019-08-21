2920abe01b42aeaaea3bb31ba412500c77c97692
周公来 <JohnZhou@nn.com>
2019-08-21 16:56:49 +0800

    type: update

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="app">
-    vuex
+    <form class="fiel-form" ref="form">
+      <div class="fiel-input" v-for="(val, key, index) in form" :key="key">
+        <label :for="`${key}-${index}`" v-if="val.inline" v-text="val.label" />
+        <input
+          :name="key"
+          v-model="val.value"
+          :type="val.inputType ? val.inputType : 'text'"
+          :id="`${key}-${index}`"
+        />
+      </div>
+      <button type="submit">submit</button>
+    </form>
   </div>
 </template>
 <script>
-import { mapState } from 'vuex';
+import { mapState } from "vuex";
 export default {
   name: "App",
   computed: {
-    ...mapState(['role', 'form'])
+    ...mapState(["role", "form"])
   }
 };
 </script>
